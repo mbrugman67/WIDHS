5a98f66b814c2d026d50f90f65d22e3b81fcdcf0
git <mattb@linux.com>
2020-12-27 11:01:59 -0600

    adding --days parameter

--- dhsWI.py
+++ dhsWI.py
@@ -25,38 +25,54 @@ import utils        # logging, debug, etc.
 # class that handles queries against the DHS database
 class dhs(object):
     # __init__() method called when an instance of the class is created
-    def __init__(self):
+    def __init__(self, history):
         utils.dbgPrint(utils.LOG_INFO, 'instantiating')
         self.req = None
         self._response = None
 
+        self._month = history.tm_mon
+        self._day = history.tm_mday
+        self._year = history.tm_year
+
         # The DHS API has 2 urls we are interested in: 11 & 12.  11 gives us the whole state, 12 by county
-        self._urlState = 'https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_WI/MapServer/11/query?where=DATE%3E%272020-11-24%2014%3A00%3A00.000000%27&outFields=DATE,POS_NEW,TEST_NEW,NAME,DTH_NEW,HOSP_YES&outSR=4326&f=json'
-        self._urlCounty = 'https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_WI/MapServer/12/query?where=NAME%3D%27COUNTY%27%20and%20DATE%3E%272020-11-24%2014%3A00%3A00.000000%27&outFields=NAME,DATE,POS_NEW,HOSP_YES,DTH_NEW,TEST_NEW&outSR=4326&f=json'
-        self._urlMuni = 'https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_WI/MapServer/16/query?where=NAME%3D%27MUNI%27%20and%20DATE%3E%272020-11-24%2014%3A00%3A00.000000%27&outFields=NAME,DATE,POS_NEW,HOSP_YES,DTH_NEW,TEST_NEW&outSR=4326&f=json'
+        self._urlState = 'https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_WI/MapServer/11/query?where=DATE%3E%27YEAR-MONTH-DAY%2014%3A00%3A00.000000%27&outFields=DATE,POS_NEW,TEST_NEW,NAME,DTH_NEW,HOSP_YES&outSR=4326&f=json'
+        self._urlCounty = 'https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_WI/MapServer/12/query?where=NAME%3D%27COUNTY%27%20and%20DATE%3E%27YEAR-MONTH-DAY%2014%3A00%3A00.000000%27&outFields=NAME,DATE,POS_NEW,HOSP_YES,DTH_NEW,TEST_NEW&outSR=4326&f=json'
+        self._urlMuni = 'https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_WI/MapServer/16/query?where=NAME%3D%27MUNI%27%20and%20DATE%3E%27YEAR-MONTH-DAY%2014%3A00%3A00.000000%27&outFields=NAME,DATE,POS_NEW,HOSP_YES,DTH_NEW,TEST_NEW&outSR=4326&f=json'
     
     # Do the actual query, the result is a blob of JSON
     def doQuery(self, county, city):
+        query = None
         if county == None and city == None:
             # basic query for the whole state
-            self._response = requests.get(self._urlState)
+            query = self._urlState.replace('YEAR', str(self._year))
+            query = query.replace('MONTH', str(self._month))
+            query = query.replace('DAY', str(self._day))
+
+            # display county name for results
+            utils.outline('Query for entire state')
+            
         elif city == None:
             # specific county; replace the text COUNTY with desired county name
             query = self._urlCounty.replace('COUNTY', county)
-            self._response = requests.get(query)
-            utils.dbgPrint(utils.LOG_DBG, query)
+            query = query.replace('YEAR', str(self._year))
+            query = query.replace('MONTH', str(self._month))
+            query = query.replace('DAY', str(self._day))
 
             # display county name for results
             utils.outline('Query for {} county'.format(county))
         else:
             # specify municipality; replace text MUNI with desired municipality name
             query = self._urlMuni.replace('MUNI', city)
-            self._response = requests.get(query)
-            utils.dbgPrint(utils.LOG_DBG, query)
+            query = query.replace('YEAR', str(self._year))
+            query = query.replace('MONTH', str(self._month))
+            query = query.replace('DAY', str(self._day))
 
             # display municipality name for results
             utils.outline('Query for municipality "{}"'.format(city))
         
+        utils.dbgPrint(utils.LOG_INFO, 'query string: {}'.format(query))
+        self._response = requests.get(query)
+
         utils.dbgPrint(utils.LOG_INFO, 'raw output: {}'.format(self._response.text))
 
         # check for bad response; server busy, bad county name, etc
@@ -163,8 +179,10 @@ class dhs(object):
             except IndexError:
                 pass
 
-            utils.outline('14-day positivity rate: {:> .1f}% ({} positive of {} tests)'.format(
-                ((positive / total) * 100), positive, total))
+            # don't show 14 day rate if less than 14 days of data.  duh.
+            if count14 == 14:
+                utils.outline('14-day positivity rate: {:> .1f}% ({} positive of {} tests)'.format(
+                    ((positive / total) * 100), positive, total))
 
 ########################################################################
 ###                              MAIN                                ###
@@ -176,6 +194,7 @@ if __name__ == "__main__":
     parser.add_argument('--outfile', dest='outfile', default=None, required=False, help='name of output file')
     parser.add_argument('--county', dest='county', default=None, required=False, help='specific county')
     parser.add_argument('--muni', dest='muni', default=None, required=False, help='specific municipality')
+    parser.add_argument('--days', dest='days', default=15, type=int, required=False, help='how many previous days')
     args = parser.parse_args()
     
     utils.setLogLevel(args.verbosity)
@@ -188,8 +207,18 @@ if __name__ == "__main__":
     if utils.LOG_LEVEL > utils.LOG_ERR:
         utils.outline('Output level: {}'.format(utils.logLevelToText()))
 
+    # how far back to go in history?
+    previous = time.localtime(
+        time.time() - datetime.timedelta.total_seconds(datetime.timedelta(days = args.days + 1)))
+
+    # we lose a couple of days accumulating data between days, so lie to the user
+    display = time.localtime(
+        time.time() - datetime.timedelta.total_seconds(datetime.timedelta(days = args.days - 1)))
+
+    utils.outline('Getting data from {} to now'.format(time.strftime('%m/%d/%Y', display)))
+
     # new query object
-    DHS = dhs()
+    DHS = dhs(previous)
 
     # do the query
     DHS.doQuery(args.county, args.muni)
